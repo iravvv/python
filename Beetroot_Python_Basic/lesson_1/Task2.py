f949b4faeb00500c95b71c3b40cc26af4e75bc4d
iravvv <136954433+iravvv@users.noreply.github.com>
2023-08-29 21:03:26 +0300

    lesson_4

--- Beetroot_Python_Basic/lesson_1/Task2.py
+++
@@ -1,7 +0,0 @@
-print("hello")# Вивести текст hello
-print("10")# Вивести 10
-print("10, 12")#Вивести текст 10, 12
-print(10, 12, sep='+') #Вивести текст 10+12
-print("Iryna", end='\n')#Вивести текст Iryna Hunko Alexandrovna в різних стрічках
-print("Hunko", end='\n')
-print("Alexadrovna", end='\n')
